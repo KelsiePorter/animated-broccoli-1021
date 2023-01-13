3e04481de884ececc7c5dc82e936c4931f2ec574
Kelsie Porter <krporter22@gmail.com>
2023-01-13 10:55:45 -0700

    Complete US 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   #doctors
   get '/doctors/:id', to: 'doctors#show'
   delete '/doctors/:doctor_id/patients/:patient_id', to: 'doctor_patients#destroy'
+
+  #patients
+  get '/patients', to: 'patients#index'
 end
