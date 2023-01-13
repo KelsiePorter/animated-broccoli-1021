5af8d34e8e2c37a06b135725d519d3589ea2a469
Kelsie Porter <krporter22@gmail.com>
2023-01-13 11:16:34 -0700

    Update test name

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Patient do
       @patient_7 = Patient.create!(name: "Katie", age: 17)
     end
 
-    it 'returns all patients older than 18 in acending order' do 
+    it 'returns all patients older than 18 in alphabetical ascending order' do 
       expect(Patient.patients_over_18_acending).to eq([@patient_1, @patient_3, @patient_2, @patient_4])
       expect(Patient.patients_over_18_acending).to_not eq([@patient_6, @patient_7, @patient_5])
     end
