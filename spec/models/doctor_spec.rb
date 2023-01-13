a3a412486c393d8e4ac17e6c425fe41cea0bcdc2
Kelsie Porter <krporter22@gmail.com>
2023-01-13 11:44:17 -0700

    Create #patient_count and associated test

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,4 +6,51 @@ RSpec.describe Doctor do
     it {should have_many :doctor_patients}
     it {should have_many(:patients).through(:doctor_patients)}
   end
+
+  describe 'instance methods' do 
+    before :each do 
+      @hospital_1 = Hospital.create!(name: "Children's")
+      @doctor_1 = Doctor.create!(name: "River", specialty: "dermatology", university: "CU", hospital_id: @hospital_1.id)
+      @doctor_2 = Doctor.create!(name: "Bodi", specialty: "cardiology", university: "MSU", hospital_id: @hospital_1.id)
+      @doctor_3 = Doctor.create!(name: "Mable", specialty: "radiology", university: "CSU", hospital_id: @hospital_1.id)
+      
+      @hospital_2 = Hospital.create!(name: "Lutheran")
+      @doctor_4 = Doctor.create!(name: "Doby", specialty: "Emergency", university: "CC", hospital_id: @hospital_2.id)
+  
+      @patient_1 = Patient.create!(name: "Dean", age: 55)
+      @patient_2 = Patient.create!(name: "Tayo", age: 33)
+      @patient_3 = Patient.create!(name: "Moses", age: 47)
+      @patient_4 = Patient.create!(name: "Tyler", age: 23)
+      @patient_5 = Patient.create!(name: "Madison", age: 18)
+      @patient_6 = Patient.create!(name: "Chris", age: 10)
+      @patient_7 = Patient.create!(name: "Katie", age: 17)
+  
+      @doctor_1.patients << @patient_1
+      @doctor_1.patients << @patient_2
+      @doctor_1.patients << @patient_5
+      @doctor_1.patients << @patient_6
+      @doctor_1.patients << @patient_7
+  
+      @doctor_2.patients << @patient_1
+      @doctor_2.patients << @patient_2
+      @doctor_2.patients << @patient_3
+      @doctor_2.patients << @patient_4
+      @doctor_2.patients << @patient_5
+      @doctor_2.patients << @patient_6
+  
+      @doctor_3.patients << @patient_1
+      @doctor_3.patients << @patient_2
+      @doctor_3.patients << @patient_3
+      @doctor_3.patients << @patient_4
+      @doctor_3.patients << @patient_5
+      @doctor_3.patients << @patient_6
+      @doctor_3.patients << @patient_7
+    end
+    it 'returns the number of patients for a doctor' do 
+
+      expect(@doctor_1.patient_count).to eq(5)
+      expect(@doctor_2.patient_count).to eq(6)
+      expect(@doctor_3.patient_count).to eq(7)
+    end
+  end
 end
