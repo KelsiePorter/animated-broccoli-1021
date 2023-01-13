647616dda64d148fca9291d9dafae8eee7bdaeca
Kelsie Porter <krporter22@gmail.com>
2023-01-13 10:55:19 -0700

    Create class method for patients_over_18_acending

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Patient do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
   end
+
+  describe 'class methods' do 
+    before :each do 
+      @patient_1 = Patient.create!(name: "Dean", age: 55)
+      @patient_2 = Patient.create!(name: "Tayo", age: 33)
+      @patient_3 = Patient.create!(name: "Moses", age: 47)
+      @patient_4 = Patient.create!(name: "Tyler", age: 23)
+      @patient_5 = Patient.create!(name: "Madison", age: 18)
+      @patient_6 = Patient.create!(name: "Chris", age: 10)
+      @patient_7 = Patient.create!(name: "Katie", age: 17)
+    end
+
+    it 'returns all patients older than 18 in acending order' do 
+      expect(Patient.patients_over_18_acending).to eq([@patient_1, @patient_3, @patient_2, @patient_4])
+    end
+  end
 end
