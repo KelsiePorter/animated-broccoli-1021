31bc948f32c552deb1082c908ae49d82f0bc0536
Kelsie Porter <krporter22@gmail.com>
2023-01-13 11:06:20 -0700

    Move .patients_over_18_acending to patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,7 @@
 class PatientsController < ApplicationController
   
   def index
-    @patients = Patient.all
+    patients = Patient.all
+    @patients = patients.patients_over_18_acending
   end
 end
